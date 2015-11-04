ca0bca0033c9da4fa41efc61bcdee546aad13f2e
Lillemor <lillemorb@gmail.com>
2015-11-04 10:00:38 +0100

    Abstrakt klass DrawableElements skapad

--- Elements.cc
+++ Elements.cc
@@ -1,36 +1,52 @@
 #include <SFML/Graphics.hpp>
 
-class Player
+//TODO: flytta using namespace till .cc-fil när vi delar upp i .h/.cc
+using namespace std;
+
+class DrawableElement
 {
 public:
-  Player()
-  {
-    rectangle_.setSize(sf::Vector2f(32,32));
-    rectangle_.setFillColor(sf::Color(255, 0, 0));
-  }
-
-  void setPosition(int x, int y) { rectangle_.setPosition(x*32, y*32); }
+  virtual ~DrawableElement() = default;
+  virtual string getSpriteID() = 0;
   void draw(sf::RenderWindow& window){ window.draw(rectangle_); }
 
-private:
+protected:
   sf::RectangleShape rectangle_;
-
 };
 
 
-class Ground
+class Player : public DrawableElement
+{
+public:
+  Player()
+    {
+      rectangle_.setSize(sf::Vector2f(32,32));
+      rectangle_.setFillColor(sf::Color(255, 0, 0));
+    }
+  virtual ~Player() = default;
+
+  string getSpriteID() override
+    {
+      return "Player";
+    }
+
+  void setPosition(int x, int y) { rectangle_.setPosition(x*32, y*32); }
+};
+
+
+class Ground : public DrawableElement
 {
 public:
   
   Ground(int TILESIZE, int x, int y) 
-  { 
-    rectangle_.setPosition(x*TILESIZE, y*TILESIZE); 
-    rectangle_.setSize(sf::Vector2f(32,32));
-    rectangle_.setFillColor(sf::Color(150, 255, 100));
-  }
-
-  void draw(sf::RenderWindow& window){ window.draw(rectangle_); }
-
-private:
-  sf::RectangleShape rectangle_;
+    { 
+      rectangle_.setPosition(x*TILESIZE, y*TILESIZE); 
+      rectangle_.setSize(sf::Vector2f(32,32));
+      rectangle_.setFillColor(sf::Color(150, 255, 100));
+    }
+  virtual ~Ground() = default;
+  string getSpriteID() override
+    {
+      return "Ground";
+    }
 };
--- Level.cc
+++ Level.cc
@@ -3,30 +3,30 @@
 
 using namespace std;
 
-
 class Level
 {
 public:
   Level(int TILESIZE, int TILES_PER_ROW, int TILES_PER_COLUMN, vector<int> level_vector)
-  {
-    for(int i{}; i < level_vector.size(); i++)
+    {
+
+      for(unsigned int i{}; i < level_vector.size(); i++)
       {
 	int x = i%TILES_PER_ROW;
 	int y = i/TILES_PER_ROW;
 
 	switch(level_vector.at(i))
-	  {
-	  case 01:
-	    ground_vector_.push_back(Ground(TILESIZE, x, y));
-	    break;
-	  case 02:
-	    player_.setPosition(x, y);
-	    break;
-	  default:
-	    break;
-	  }
+	{
+	case 01:
+	  ground_vector_.push_back(Ground(TILESIZE, x, y));
+	  break;
+	case 02:
+	  player_.setPosition(x, y);
+	  break;
+	default:
+	  break;
+	}
       }
-  }
+    }
     
 
   ~Level() = default;
@@ -35,13 +35,13 @@ public:
   //vector<PhysicalElement*> getLevelPhysicalLayout();
   
   void draw(sf::RenderWindow& window)
-  {
-    player_.draw(window);
-    for(int i{}; i < ground_vector_.size(); i++)
+    {
+      player_.draw(window);
+      for(unsigned int i{}; i < ground_vector_.size(); i++)
       {
 	ground_vector_.at(i).draw(window);
       }
-  }
+    }
 
 
 private:
