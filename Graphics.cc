cd1c3c1b615dab43c56cdef0f9aa0e11de526d68
Daniel Peterson <danpe807@li17-23.ida.liu.se>
2015-11-05 11:47:51 +0100

    Sprites är tillagda

--- Graphics.cc
+++ Graphics.cc
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+
 // Todo: Ändra inkludering då filer delas upp i .h/.cc
 //#include "Elements.cc"
 #include "Level.cc"
@@ -10,12 +11,46 @@ using namespace std;
 class Graphics
 {
 public:
+  
   void drawLevel(Level current, sf::RenderWindow& window)
     {
       vector<DrawableElement*> levelVec(current.getLevelDrawableLayout());
 
-      sf::RectangleShape rectangle_;
-      rectangle_.setSize(sf::Vector2f(32,32));
+      // Går detta att flytta ut på ett snyggt sätt?
+      int TILESIZE = 32;
+
+      // Dessa ska bara göras en gång
+      sf::Texture spriteSheet_background;
+      if (!spriteSheet_background.loadFromFile("Sprites/Tiles.png"))
+	{
+	  cerr << "Kunde inte ladda backgrounds sprites" << endl;
+	  // Fixa felhantering! 
+	  // return 1;
+	}
+
+      sf::Texture spriteSheet_player;
+      if (!spriteSheet_player.loadFromFile("Sprites/Tomato.png")) 
+	{
+	  cerr << "Kunde inte ladda players sprite" << endl;
+	  // Fixa felhantering! 
+	  //return 1;
+	}
+
+      sf::Sprite player_sprite;
+      player_sprite.setTexture(spriteSheet_player);
+      player_sprite.setTextureRect(sf::IntRect(0, 0, TILESIZE, TILESIZE));
+      sf::Sprite ground_sprite;
+      ground_sprite.setTexture(spriteSheet_background);
+      ground_sprite.setTextureRect(sf::IntRect(32, 0, TILESIZE, TILESIZE));
+      sf::Sprite door_sprite;
+      door_sprite.setTexture(spriteSheet_background);
+      door_sprite.setTextureRect(sf::IntRect(96, 0, TILESIZE, TILESIZE));
+      sf::Sprite block_sprite;
+      block_sprite.setTexture(spriteSheet_background);
+      block_sprite.setTextureRect(sf::IntRect(0, 0, TILESIZE, TILESIZE));
+
+
+
       for(unsigned int i{}; i < levelVec.size(); i++)
       {
 	string tempID = levelVec.at(i)->getSpriteID();
@@ -23,16 +58,28 @@ public:
 	  
 	if (tempID == "Player")
 	{
-	  rectangle_.setFillColor(sf::Color(255, 0, 0)); 
-	  rectangle_.setPosition(tempPos.x,tempPos.y);	  
+	  player_sprite.setPosition(tempPos.x, tempPos.y);
+	  window.draw(player_sprite);
 	}
 	else if (tempID == "Ground") 
 	{
-	  rectangle_.setFillColor(sf::Color(150, 255, 100));
-	  rectangle_.setPosition(tempPos.x,tempPos.y);
+	  ground_sprite.setPosition(tempPos.x, tempPos.y);
+	  window.draw(ground_sprite);
 	}
-	window.draw(rectangle_);
+	else if (tempID == "Door") 
+	{
+	  door_sprite.setPosition(tempPos.x, tempPos.y);
+	  window.draw(door_sprite);
+	}
+	else if (tempID == "Block") 
+	{
+	  block_sprite.setPosition(tempPos.x, tempPos.y);
+	  window.draw(block_sprite);
+	}
       }
     }
+
+private:
+
 
 };
