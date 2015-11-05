3e9fc6cb1a12454efc2320f38843d04e9bcc93eb
Daniel Peterson <danpe807@li11-4.ida.liu.se>
2015-11-05 16:47:33 +0100

    Pause, Escape och halvkass ifstream

--- Game.cc
+++ Game.cc
@@ -3,6 +3,8 @@
 #include "Logic.cc"
 #include "Graphics.cc"
 #include <vector>
+#include <fstream>
+#include <iterator>
 using namespace std;
 
 // TODO: dela upp i .cc/.h-filer
@@ -22,28 +24,34 @@ int run()
   // Skapa fönster som är 768x576 pixlar (är delbart på 32), går ej att resizea
   sf::RenderWindow window(sf::VideoMode(768, 576), "Come on, catch up!", sf::Style::Titlebar | sf::Style::Close);
   
-  // Level 1
   vector<int> level1{
     00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
       00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
       00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
       00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
+      00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 
       00, 00, 00, 00, 00, 00, 00, 00, 00, 02, 00, 00, 04, 00, 00, 00, 00, 03, 00, 00, 00, 00, 00, 00,
       01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01,
       01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01,
       01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01
       };
-  
+  /*
+  ifstream is("level1.txt");
+  istream_iterator<int> start(is), end;
+  // Level 1
+  vector<int> level1(istream_iterator<int> {is});
+  */
+
+
   // Pga problem med att Level deletar sina pekare då ett tillfälligt objekt
   // tas bort, håller vi reda på aktuell Level med en pekare istället
   // Medans vi fortfarande bara har en Level
@@ -79,11 +87,22 @@ int run()
 	      //Hoppa
 	      action = Jump;
 	      }*/
-	  else if (event.type == sf::Event::Closed)
+	  else if (event.type == sf::Event::Closed || 
+		   sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 	    {
 	      // "close requested" event: we close the window
 	      window.close();
 	    }
+	  /*
+	  else if (event.type == sf::Event::LostFocus)
+	    {
+	      GameState = Pause;
+	    }
+	  else if (event.type == sf::Event::GainedFocus)
+	    {
+	      GameState = Playing;
+	    }
+	  */
 	  else
 	    {
 	      action = Logic::Nothing;
@@ -111,8 +130,9 @@ private:
   const int TILES_PER_ROW{24};
 //  const int TILES_PER_COLUMN{18};
   unsigned int current_level{0};
-  enum GameState{Playing, ShowScreen, Exit};
-  enum ActionResult{Nothing, GameCompleted, LevelCompleted, Dead, Reset, Quit};
+  enum GameState{Playing, ShowScreen, Pause, Exit};
+// enum actionresult finns redan i logic
+  enum ActionResult{Nothing, GameCompleted, LevelCompleted, Dead, Reset, Quit};  
   GameState gamestate_{Playing};
   ActionResult actionResult_{Nothing};
 //  vector<Level> level_vector_{};
