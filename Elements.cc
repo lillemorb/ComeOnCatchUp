6dd42a9bbe04bd0e1d4114a5c9775e04320e049e
Lillemor Blom <lillemorb@gmail.com>
2015-11-29 15:01:58 +0100

    Rensat ytterligare i Elements (forts nedan)
    Rensat ytterligare i Elements, tagit bort namespace std och onödiga includes.
    
    Gjort om så att flera funktioner som var pure virtual i framför allt PhysicalElement
    istället är bara virtual, med en implementation i PhysicalElement utan effekt
    men implementation med effekt för de klasser som ska påverkas av dem. Ex: move.

--- Elements.cc
+++ Elements.cc
@@ -1,17 +1,34 @@
+#include "Elements.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include "Elements.h"
 
-//TODO: flytta using namespace till .cc-fil när vi delar upp i .h/.cc
-//TODO: städa funktioner, var ska de ligga, uppdatera övriga filer därefter
 using namespace std;
 
+//---------DRAWABLE--------------//
+DrawableElement::~DrawableElement() = default;
+
+//---------PHYSICAL--------------//
+PhysicalElement::~PhysicalElement() = default;
+
+//---------PLAYER--------------//
+Player::Player(int TILESIZE, float x, float y)
+	: PhysicalElement("Player")
+{
+	rectangle_.setPosition(x*TILESIZE, y*TILESIZE);      
+	rectangle_.setSize(sf::Vector2f(TILESIZE,TILESIZE));
+}
+
 // Returnerar en mindre bounding box för player än tilesize
-sf::FloatRect Player::getGlobalBounds() {
+sf::FloatRect Player::getGlobalBounds() const {
 	sf::FloatRect largeBounds{rectangle_.getGlobalBounds()};
 	sf::FloatRect smallerBounds(largeBounds.left+4.0, largeBounds.top,
 															largeBounds.width-8.0, largeBounds.height);
 	return smallerBounds;
+}
+
+sf::Vector2f Player::getSize() const
+{
+	return sf::Vector2f(getGlobalBounds().width, getGlobalBounds().height);
 }
 
 void Player::setAnimationcycle() { 
@@ -51,19 +68,25 @@ sf::Vector2f Player::getAnimation()
 	return currentSprite_;
 }
 
+//---------BLOCK--------------//
 Block::Block(int TILESIZE, int x, int y) 
 	: PhysicalElement("Block")
 { 
 	rectangle_.setPosition(x*TILESIZE, y*TILESIZE); 
 	rectangle_.setSize(sf::Vector2f(TILESIZE,TILESIZE));      
 }
-Ground::Ground(int TILESIZE, int x, int y, string elementID, CollisionBorders collisionBorders)
+
+//---------GROUND--------------//
+Ground::Ground(int TILESIZE, int x, int y, string elementID,
+							 CollisionBorders collisionBorders)
 	: PhysicalElement(elementID)
 { 
 	collisionBorders_ = collisionBorders;
 	rectangle_.setPosition(x*TILESIZE, y*TILESIZE); 
 	rectangle_.setSize(sf::Vector2f(TILESIZE,TILESIZE));
 }
+
+//---------DOOR--------------//
 Door::Door(int TILESIZE, int x, int y)
 	: PhysicalElement("Door")
 {
@@ -71,13 +94,15 @@ Door::Door(int TILESIZE, int x, int y)
 	rectangle_.setSize(sf::Vector2f(TILESIZE,TILESIZE));
 }
 
-sf::FloatRect Door::getGlobalBounds() {
+// returnerar liten bounding box så att man ser Player "gå in i" dörren
+sf::FloatRect Door::getGlobalBounds() const {
 	sf::FloatRect largeBounds{rectangle_.getGlobalBounds()};
 	sf::FloatRect smallerBounds(largeBounds.left+15.0, largeBounds.top+26.0,
 															largeBounds.width-30.0, largeBounds.height-26.0);
 	return smallerBounds;
 }
 
+//---------BACKGROUND--------//
 Background::Background(int TILESIZE, int x, int y, string elementID)
 	: DrawableElement(elementID)
 {
--- Elements.h
+++ Elements.h
@@ -2,21 +2,18 @@
 #define ELEMENTS_H
 
 #include <SFML/Graphics.hpp>
-#include <iostream>
-
-using namespace std;
 
 //---------DRAWABLE--------------//
 class DrawableElement
 {
 public:
-  DrawableElement(string elementID) : elementID_{elementID} {};
-  virtual ~DrawableElement() = default;
-  string getElementID(){ return elementID_; }
-  sf::Vector2f getPosition() { return rectangle_.getPosition(); }
+DrawableElement(std::string elementID) : elementID_{elementID} {};
+  virtual ~DrawableElement() = 0;
+	std::string getElementID() const { return elementID_; }
+  sf::Vector2f getPosition() const { return rectangle_.getPosition(); }
 
 protected:
-  string elementID_{};
+	std::string elementID_{};
   sf::RectangleShape rectangle_;
 };
 
@@ -24,33 +21,26 @@ protected:
 class PhysicalElement : public DrawableElement
 {
 public:
-  PhysicalElement(string elementID): DrawableElement(elementID){}
-  virtual ~PhysicalElement() = default;
+PhysicalElement(std::string elementID): DrawableElement(elementID){}
+  virtual ~PhysicalElement() = 0;
  
   struct CollisionBorders
   { bool left{true}; bool right{true}; bool up{true}; bool down{true}; };
 
-  //Följande funktioner implementeras som tomma för de objekt
-  //som de inte berör
-  virtual void setVelocity(sf::Vector2f vel)  = 0;
-  virtual void setPosition(sf::Vector2f pos)  = 0;
-  virtual void move(const sf::Vector2f offset) = 0; 
-
-  //Alla ärvande klasser implementerar egen variant
-  virtual sf::FloatRect getGlobalBounds() = 0;
-
   //Implementeras här men ärvande klasser kan implementera egen variant
-  //( i Player returnerar den storleken på bounding box ist f rectangle )
-  virtual const sf::Vector2f getSize(){ return rectangle_.getSize();}
-
-  virtual void setBelowWindow(bool) {}
-  virtual bool getBelowWindow() { return false; }
+  virtual void setVelocity(const sf::Vector2f vel){ velocity_ = sf::Vector2f(0,gravity_); }
+  virtual void setPosition(const sf::Vector2f pos) {}
+  virtual void move(const sf::Vector2f offset) {} 
+  virtual sf::FloatRect getGlobalBounds() const { return rectangle_.getGlobalBounds(); }
+  virtual sf::Vector2f getSize() const { return rectangle_.getSize();}
+  virtual void setBelowWindow(const bool) {}
+  virtual bool getBelowWindow() const { return false; }
 
   //Implementeras här och ärvs sedan utan polymorfi
   sf::Vector2f getVelocity() { return velocity_; }
-  void setOnGround(bool onGround) { onGround_ = onGround; }
-  bool getOnGround() { return onGround_; }
-  CollisionBorders getCollisionBorders() { return collisionBorders_; }
+  void setOnGround(const bool onGround) { onGround_ = onGround; }
+  bool getOnGround() const { return onGround_; }
+  CollisionBorders getCollisionBorders() const { return collisionBorders_; }
 
 protected:
   CollisionBorders collisionBorders_{};
@@ -63,37 +53,28 @@ protected:
 class Player : public PhysicalElement
 {
 public:
-  //Player(int TILESIZE, int x, int y)
-  Player(int TILESIZE, float x, float y)
-    : PhysicalElement("Player")
-    {
-      rectangle_.setPosition(x*TILESIZE, y*TILESIZE);      
-      rectangle_.setSize(sf::Vector2f(TILESIZE,TILESIZE));
-    }
+  Player(int TILESIZE, float x, float y);
   ~Player() = default;
 
-  void setVelocity(sf::Vector2f vel) override { velocity_ = vel; } 
-  void setPosition(sf::Vector2f pos) override { rectangle_.setPosition(pos.x, pos.y); }
+  void setVelocity(const sf::Vector2f vel) override { velocity_ = vel; } 
+  void setPosition(const sf::Vector2f pos) override { rectangle_.setPosition(pos.x, pos.y); }
   void move(const sf::Vector2f offset) override { rectangle_.move(offset); }
-
-  // Returnerar en mindre bounding box för player än tilesize
-  sf::FloatRect getGlobalBounds() override;
-  virtual const sf::Vector2f getSize(){ return sf::Vector2f(getGlobalBounds().width,
-							    getGlobalBounds().height); }
+  sf::FloatRect getGlobalBounds() const override;
+  sf::Vector2f getSize() const override;
 
   //Funktioner unika för Player
-  float getGravity() { return gravity_; }
-  void setFacingRight(bool facingRight) { facingRight_ = facingRight; }
-  bool getFacingRight() { return facingRight_; }
-  void resetCurrentSprite() { currentSprite_.x = 0; }
-  sf::Vector2f getCurrentSprite() { return currentSprite_; }
-  void setJump(bool jump) { jump_ = jump; }
-  void setFalling(bool falling) { falling_ = falling; }
-  void setDeath(bool death) { death_ = death; }
-  bool getDeath() { return death_; }
-  bool getJumpAllowed() { return jumpAllowed_; }
-  void setJumpAllowed(bool jumpAllowed) { jumpAllowed_ = jumpAllowed; }
-  void setWalk(bool walk) { walk_ = walk; }
+  float getGravity() const { return gravity_; }
+  void setFacingRight(const bool facingRight) { facingRight_ = facingRight; }
+  bool getFacingRight() const { return facingRight_; }
+  void resetCurrentSprite(){ currentSprite_.x = 0; }
+  sf::Vector2f getCurrentSprite() const { return currentSprite_; }
+  void setJump(const bool jump) { jump_ = jump; }
+  void setFalling(const bool falling) { falling_ = falling; }
+  void setDeath(const bool death) { death_ = death; }
+  bool getDeath() const { return death_; }
+  bool getJumpAllowed() const { return jumpAllowed_; }
+  void setJumpAllowed(const bool jumpAllowed) { jumpAllowed_ = jumpAllowed; }
+  void setWalk(const bool walk) { walk_ = walk; }
   void setAnimationcycle();
   sf::Vector2f getAnimation();
 
@@ -120,13 +101,13 @@ public:
   Block(int TILESIZE, int x, int y);
   ~Block() = default;
 
-  void setVelocity(sf::Vector2f vel) override { velocity_ = vel; } 
-  void setPosition(sf::Vector2f pos) override { rectangle_.setPosition(pos.x, pos.y); }
+  void setVelocity(const sf::Vector2f vel) override { velocity_ = vel; } 
+  void setPosition(const sf::Vector2f pos) override
+	{ rectangle_.setPosition(pos.x, pos.y); }
   void move(const sf::Vector2f offset) override { rectangle_.move(offset); }
-  sf::FloatRect getGlobalBounds() override { return rectangle_.getGlobalBounds(); }
-  
-  void setBelowWindow(bool out) override { belowWindow_=out; }
-  bool getBelowWindow() override {return belowWindow_;}
+
+  void setBelowWindow(const bool out) override { belowWindow_=out; }
+  bool getBelowWindow() const override { return belowWindow_; }
 
 private:
   bool belowWindow_{false};
@@ -137,13 +118,9 @@ class Ground : public PhysicalElement
 {
 public:
   
-  Ground(int TILESIZE, int x, int y, string elementID, CollisionBorders collisionBorders);
+  Ground(int TILESIZE, int x, int y, std::string elementID,
+				 CollisionBorders collisionBorders);
   ~Ground() = default;
-
-  void setVelocity(sf::Vector2f) override { velocity_ = sf::Vector2f(0,0); } 
-  void setPosition(sf::Vector2f) override { }
-  void move(const sf::Vector2f offset) override { }
-  sf::FloatRect getGlobalBounds() override { return rectangle_.getGlobalBounds(); }
 };
 
 //---------DOOR--------------//
@@ -154,12 +131,7 @@ public:
   Door(int TILESIZE, int x, int y);
   ~Door() = default;
 
-  void setVelocity(sf::Vector2f) override { sf::Vector2f(0,0); } 
-  void setPosition(sf::Vector2f) override { }
-  void move(const sf::Vector2f offset) override { }
-
-  // returnerar liten bounding box så att man ser Player "gå in i" dörren
-  sf::FloatRect getGlobalBounds() override;
+  sf::FloatRect getGlobalBounds() const override;
 };
 
 //---------BACKGROUND--------//
@@ -167,7 +139,7 @@ class Background : public DrawableElement
 {
 public:
 
-  Background(int TILESIZE, int x, int y, string elementID);
+  Background(int TILESIZE, int x, int y, std::string elementID);
   ~Background() = default;
 };
 
